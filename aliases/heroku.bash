8cd44f6b3f7a1a511aff117f8efedfdc629b9045
Eric Rochester <err8n@virginia.edu>
2012-03-01 14:16:40 -0500

    Added modified heroku aliases.

---
+++ aliases/heroku.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# This is taken from bash_it, but I've changed any that
+# conflict with my hg aliases.
+
+# heroku
+alias her='heroku'
+alias hls='heroku list'
+alias hi='heroku info'
+alias ho='heroku open'
+
+# dynos and workers
+alias hd='heroku dynos'
+alias hw='heroku workers'
+
+# rake console
+alias hr='heroku rake'
+alias hcon='heroku console'
+
+# new and restart
+alias hnew='heroku create'
+alias hrestart='heroku restart'
+
+# logs
+alias hlog='heroku logs'
+alias hlogs='heroku logs'
+
+# maint
+alias hon='heroku maintenance:on'
+alias hoff='heroku maintenance:off'
+
+# heroku configs
+alias hc='heroku config'
+alias hca='heroku config:add'
+alias hcr='heroku config:remove'
+alias hcc='heroku config:clear'
+
+function heroku-help() {
+	afor heroku
+}
