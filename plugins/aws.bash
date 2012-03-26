5edd8ec75e4decac952ca7128a2e934d3b78c4a1
Eric Rochester <err8n@virginia.edu>
2012-03-26 16:47:12 -0400

    Added env-vars for Amazon EC2 tools.

---
+++ plugins/aws.bash
@@ -0,0 +1,11 @@
+
+#export JAVA_HOME="$(/usr/libexec/java_home)"
+
+if [ -d $HOME/.ec2 ] ; then
+	export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+	export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
+fi
+
+export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
+
+
