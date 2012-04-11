ec60ada7d5a7328737c60ac72c9ab894ab609a78
Eric Rochester <err8n@virginia.edu>
2012-04-11 12:35:51 -0400

    Added quilt aliases.

---
+++ aliases/quilt.bash
@@ -0,0 +1,38 @@
+
+# A collection of aliases for quilt.
+
+alias q='quilt'
+alias qadd='quilt add'
+alias qann='quilt annotate'
+alias qapp='quilt applied'
+# alias qd='quilt delete'
+alias qd='quilt diff'
+alias qed='quilt edit'
+alias qf='quilt files'
+alias qfd='quilt fold'
+alias qfk='quilt fork'
+alias qgr='quilt graph'
+alias qg='quilt grep'
+alias qh='quilt header'
+alias qi='quilt import'
+# alias qm='quilt mail'
+alias qnew='quilt new'
+alias qnx='quilt next'
+alias qpt='quilt patches'
+alias qpop='quilt pop'
+alias qpv='quilt previous'
+alias qpush='quilt push'
+alias qref='quilt refresh'
+alias qrn='quilt rename'
+alias qrv='quilt revert'
+alias qser='quilt series'
+alias qsp='quilt setup'
+alias qss='quilt snapshot'
+alias qt='quilt top'
+alias qu='quilt unapplied'
+# alias qup='quilt upgrade'
+
+function quilt-help() {
+	afor quilt
+}
+
