e9be38dc4cff4b71583c6b85c3c3ce8ef0d30ff7
Eric Rochester <err8n@virginia.edu>
2012-04-16 10:04:25 -0400

    Added Python 3 installation directory to the path.

--- plugins/python.bash
+++ plugins/python.bash
@@ -1,6 +1,6 @@
 
 PYTHONPATH=$HOME/lib/python:/usr/local/lib/python2.7/site-packages
-PATH=/usr/local/share/python:$PATH
+PATH=/usr/local/share/python:/usr/local/share/python3:$PATH
 
 export PYTHONPATH
 
