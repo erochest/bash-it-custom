86a475d8b45be6ae4db53eec9bc3c1351faf294e
Inderpreet Singh <inderpreet.singh@libertymutual.com>
2017-12-19 10:49:02 -0500

    Allow running install.sh from anywhere.

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-grep -q -F 'BASH_IT_CUSTOM' ~/.bash_profile ~/.bashrc || echo "BASH_IT_CUSTOM=$PWD\n\n$(cat ~/.bash_profile)" > ~/.bash_profile
+BASEDIR=$(dirname "$0")
+grep -q -F 'BASH_IT_CUSTOM' ~/.bash_profile ~/.bashrc || echo "BASH_IT_CUSTOM=$BASEDIR\n\n$(cat ~/.bash_profile)" > ~/.bash_profile
