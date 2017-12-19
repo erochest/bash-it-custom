3f2108869fcc697ccdb4fb6d92fbdcde9c1b60b0
Inderpreet Singh <inderpreet.singh@libertymutual.com>
2017-12-19 10:44:20 -0500

    Prepend BASH_IT_CUSTOM path to `.bash_profile`
    Some users may not have `.bashrc` sourced in `.bash_profile` as
    recommended by https://github.com/Bash-it/bash-it#install-options.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grep -q -F 'BASH_IT_CUSTOM' ~/.bash_profile ~/.bashrc || echo "\nBASH_IT_CUSTOM=$PWD\n" >> ~/.bashrc
+grep -q -F 'BASH_IT_CUSTOM' ~/.bash_profile ~/.bashrc || echo "BASH_IT_CUSTOM=$PWD\n\n$(cat ~/.bash_profile)" > ~/.bash_profile
